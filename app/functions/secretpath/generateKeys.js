cc2305719b79d118a7d51da89b5d09b732c8aec2
Blake Regalia <blake.regalia@gmail.com>
2025-02-24 16:27:24 -0800

    fix: ecdh and imports

--- app/functions/secretpath/generateKeys.js
+++ app/functions/secretpath/generateKeys.js
@@ -3,7 +3,7 @@
 "use client";
 import { ethers } from "ethers";
 import { arrayify, SigningKey } from "ethers/lib/utils";
-import { ecdh } from "@solar-republic/neutrino";
+import { initWasmSecp256k1 } from '@solar-republic/wasm-secp256k1';
 import { base64_to_bytes, sha256 } from "@blake.regalia/belt";
 
 export async function generateKeys() {
@@ -14,7 +14,8 @@ export async function generateKeys() {
   const gatewayPublicKey = "A20KrD7xDmkFXpNMqJn1CLpRaDLcdKpO1NdBBS7VpWh3";
   const gatewayPublicKeyBytes = base64_to_bytes(gatewayPublicKey);
 
-  const sharedKey = await sha256(ecdh(userPrivateKeyBytes, gatewayPublicKeyBytes));
+  const wasm = await initWasmSecp256k1();
+  const sharedKey = await sha256(wasm.ecdh(userPrivateKeyBytes, gatewayPublicKeyBytes));
 
   return { userPrivateKeyBytes, userPublicKeyBytes, sharedKey };
 }
